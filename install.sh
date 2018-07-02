3eb572af87d8d019da33a3b3005f784dbd19ea2d
iqhemmet <40765088+iqhemmet@users.noreply.github.com>
2018-07-02 15:30:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Get the IP address
-export IP="$(hostname -I | sed -e 's/[[:space:]]*$//')"
-
-echo -e "####################\nYour IP address is: \n# $IP\n####################"
-
 sudo apt-get update
 
 sudo apt-get install -y socat jq avahi-daemon apt-transport-https ca-certificates software-properties-common
@@ -24,4 +19,4 @@ sudo apt-get install -y docker-ce
 
 curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | sudo bash -
 
-echo -e "\nSuccess! go to\n\nhttp://$IP:8123\n"
+echo -e "\nSuccess!\n\n go to\n\nhttp://YOURSERVERIP:8123\n"
